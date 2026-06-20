314bf69de52a73f5e39e4b0c51b6e167054cda90
yousef94s <xixxii94@gmail.com>
2026-06-20 05:25:46 +0300

    Update __init__.py

--- Rose/__init__.py
+++ Rose/__init__.py
@@ -6,26 +6,25 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from pyrogram import Client
 from pyrogram.types import Message
 from Python_ARQ import ARQ
-from pyrogram import Client
 from config import Config
 import pymongo
 import pytz
 from datetime import datetime
 
-
-IST = pytz.timezone('Asia/Colombo')
-time = datetime.now(IST)
-date = time.strftime("%a/%d/%b/%Y %H:%M:%S")
+# حل مشكلة تزامن الوقت BadMsgNotification عبر استخدام التوقيت العالمي الموحد UTC
+IST = pytz.timezone('UTC')
+current_datetime = datetime.now(IST)
+date = current_datetime.strftime("%a/%d/%b/%Y %H:%M:%S")
 
 MOD_LOAD = []
 MOD_NOLOAD = []
 
 LOG_GROUP_ID = Config.LOG_GROUP_ID
+# استخدام time.time() بشكل صحيح بعد منع تداخل الأسماء
 bot_start_time = time.time()
 DB_URI = Config.BASE_DB 
 MONGO_URL = Config.MONGO_URL
 OWNER_ID = Config.OWNER_ID
-
 
 myclient = pymongo.MongoClient(DB_URI)
 dbn = myclient["supun"]
@@ -33,18 +32,19 @@ dbn = myclient["supun"]
 mongo_client = AsyncIOMotorClient(MONGO_URL)
 db = mongo_client.wbb
 
-import asyncio
+# حل مشكلة الحلقة البرمجية وجلسة الاتصال لـ aiohttp
 loop = asyncio.get_event_loop()
 aiohttpsession = ClientSession(loop=loop)
 
-#
-#arq = ARQ(Config.ARQ_API_URL,Config.ARQ_API_KEY, aiohttpsession)
+# تعطيل خدمة ARQ الميتة تماماً لمنع انهيار البوت
+# arq = ARQ(Config.ARQ_API_URL, Config.ARQ_API_KEY, aiohttpsession)
 
 bot = Client(
     "supun",
     bot_token=Config.BOT_TOKEN, 
     api_id=Config.API_ID,
-    api_hash=Config.API_HASH)
+    api_hash=Config.API_HASH
+)
 
 bot.start()
 
@@ -52,7 +52,8 @@ app = Client(
     "app2", 
     bot_token=Config.BOT_TOKEN, 
     api_id=Config.API_ID1, 
-    api_hash=Config.API_HASH1)
+    api_hash=Config.API_HASH1
+)
 app.start()
 
 x = app.get_me()
@@ -71,3 +72,4 @@ async def eor(msg: Message, **kwargs):
     )
     spec = getfullargspec(func.__wrapped__).args
     return await func(**{k: v for k, v in kwargs.items() if k in spec})
+    
